0ed3cb11236aabfc8556ccd87f26c7944b4bfe69
laxman1892 <lxmnrzl@gmail.com>
2025-02-06 13:26:56 +0545

    Models creation for cart

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
+from django.utils.timezone import timedelta, now
+from products.models import Product
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def has_expired(self):
+        """To check if the cart has expired or not."""
+        expiration_time = self.updated_at + timedelta(hours=24)
+        return now() > expiration_time
+
+    def __str__(self):
+        return f"Cart of {self.user.username}"
+
+class CartItem(models.Model):
+    cart = models.ForreignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} * {self.product.name}"
